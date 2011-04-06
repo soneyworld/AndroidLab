b4835bda855d70046939bef1e8a65777b19d984c
Till Lorentzen <t.lorentzen@tu-bs.de>
2011-04-06 20:02:01 +0200

    Projekt angelegt und erste Berechtigungen hinzugefügt, sowie CodeConvention angelegt

---
+++ src/src/de/tubs/ibr/android/ldap/sync/ViewLDAPContacts.java
@@ -0,0 +1,13 @@
+package de.tubs.ibr.android.ldap.sync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewLDAPContacts extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
