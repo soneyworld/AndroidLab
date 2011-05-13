88685564d2a92da5518d59707d6763f9dfd1057f
Christopher Gerloff <mail@christophergerloff.de>
2011-05-13 11:43:23 +0200

    Added onChangeListener for choose_account_preferences

--- src/src/de/tubs/ibr/android/ldap/core/activities/PrefActivity.java
+++ src/src/de/tubs/ibr/android/ldap/core/activities/PrefActivity.java
@@ -3,14 +3,17 @@ package de.tubs.ibr.android.ldap.core.activities;
 import de.tubs.ibr.android.ldap.R;
 import android.accounts.Account;
 import android.accounts.AccountManager;
-import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
-public class PrefActivity extends PreferenceActivity {
+public class PrefActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener{
+
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+  protected void onResume() {
+    super.onResume();
     addPreferencesFromResource(R.xml.choose_account_preferences);
     ListPreference list = (ListPreference) findPreference("selectedAccount");
     AccountManager accManager = AccountManager.get(this);
@@ -19,7 +22,19 @@ public class PrefActivity extends PreferenceActivity {
     for (int i=0;i<entries.length;i++) {
       entries[i] = accounts[i].name;
     }
+    SharedPreferences mPrefs = PreferenceManager
+    .getDefaultSharedPreferences(this);
+    String accountname = mPrefs.getString("selectedAccount", "");
     list.setEntries(entries);
     list.setEntryValues(entries);
+    list.setTitle(accountname);
+    mPrefs.registerOnSharedPreferenceChangeListener(this);
+  }
+
+  @Override
+  public void onSharedPreferenceChanged(SharedPreferences arg0, String arg1) {
+    ListPreference list = (ListPreference) findPreference("selectedAccount");
+    list.setTitle(arg0.getString(arg1, "<none selected>"));
+    
   }
 }
